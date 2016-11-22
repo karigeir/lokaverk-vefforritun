bd6103d51a217dc8eada114554d9d8f4c807b0ba
Sævar Ingi Sigurðsson <saevar43@tg-dw192.rhi.hi.is>
2016-11-22 14:40:49 +0000

    Change folder orientation

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+const index = require('./index');
 
 const app = express();
 
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
