37676adad0822b5e148cd24c6e22298cf7cb5d0d
Sævar Ingi Sigurðsson <saevar43@tg-dw192.rhi.hi.is>
2016-11-22 17:41:55 +0000

    Add frontpage w buttons

--- src/main.js
+++ src/main.js
@@ -1,23 +1,3 @@
-// $.(document).ready(function() {
-//   init;
-// })
-//
-// let init = () => {
-//   getDataCurrencyArion();
-// }
-//
-// let getDataCurrencyArion = (source="Arion") => {
-//   $.get(`http://apis.is/currency/${source}`, (data) => {
-//     let currencies = data.results;
-//     $('#currency__container').html('');
-//     currencies.map((currency)=> {
-//       $('#currency__container').append(`< tr><td>${currency.shortName}</td><td>${currency.bidValue}</td><td>${currency.askValue}</td></tr>`)
-//     });
-//   });
-// }
-//
-// init();
-
 const axios = require('axios');
 
 const baseURL = process.env.BASEURL || 'http://apis.is';
--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 router.get('/:source', (req, res, next) => {
   main.currencies(req.params.source)
   .then((result) => {
-    res.render('curr-table', { title: 'Fébreytirinn', currencies: result.data.results });
+    res.render('curr-table', { title: 'Gengi Gjaldmiðla', currencies: result.data.results });
   })
   .catch((error) => {
     res.render('error', { title: 'Skellur!', error: 'Eitthvað fór úrskeiðis :(' });
