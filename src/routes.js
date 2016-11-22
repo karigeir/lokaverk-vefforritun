d07a121249803db37047f52a017d5d710214b3e1
karigeir <karigegu@gmail.com>
2016-11-22 16:57:26 +0000

    Routes/

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ const main  = require('./main.js')
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('index', {title: 'Fébreytar'});
+  });
+
 /* GET home page. */
 router.get('/:source', function(req, res, next) {
   console.log(req.params)
   main.currencies(req.params.source)
   .then((result) => {
-    res.render('index', { title: 'Fébreytirinn', currencies: result.data.results });
+    res.render('curr-table', { title: 'Fébreytirinn', currencies: result.data.results });
   })
   .catch((error) => {
     res.render('error', { title: 'Skellur!', error });
