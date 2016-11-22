31166035b906c59fb740bb1de7a5126e648f29f5
Sævar Ingi Sigurðsson <saevar43@tg-dw192.rhi.hi.is>
2016-11-22 16:35:37 +0000

    Bakendavinna

--- src/main.js
+++ src/main.js
@@ -1,29 +1,33 @@
-$.(document).ready(function() {
-  init;
-})
-
-let init = () => {
-  getDataCurrencyArion();
-}
-
-let getDataCurrencyArion = (source="Arion") => {
-  $.get(`http://apis.is/currency/${source}`, (data) => {
-    let currencies = data.results;
-    $('#currency__container').html('');
-    currencies.map((currency)=> {
-      $('#currency__container').append(`< tr><td>${currency.shortName}</td><td>${currency.bidValue}</td><td>${currency.askValue}</td></tr>`)
-    });
-  });
-}
-
-init();
-
-// let getDataBrottfarirIs = (lang="is", type="departures") => {
-//   $.get(`http://apis.is/flight?language=${lang}&type=${type}`, (data) => {
-//     let flights = data.results;
-//     $('#flights-container').html('');
-//     flights.map((flight)=> {
-//       $('#flights-container').append(`< tr><td>${flight.date}</td><td>${flight.flightNumber}</td><td>${flight.to}</td><td>${flight.plannedArrival}</td><td>${flight.realArrival}</td></tr>`)
+// $.(document).ready(function() {
+//   init;
+// })
+//
+// let init = () => {
+//   getDataCurrencyArion();
+// }
+//
+// let getDataCurrencyArion = (source="Arion") => {
+//   $.get(`http://apis.is/currency/${source}`, (data) => {
+//     let currencies = data.results;
+//     $('#currency__container').html('');
+//     currencies.map((currency)=> {
+//       $('#currency__container').append(`< tr><td>${currency.shortName}</td><td>${currency.bidValue}</td><td>${currency.askValue}</td></tr>`)
 //     });
 //   });
 // }
+//
+// init();
+
+const axios = require('axios');
+
+const baseURL = process.env.BASEURL || 'http://apis.is';
+const instance = axios.create({ baseURL });
+
+function currencies(source) {
+  const promise = instance;
+  return promise.get(`/currency/${source}`);
+}
+
+module.exports = {
+  currencies,
+}
